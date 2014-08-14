fc0f306382a9e0338ae488d85015fc8ea34f3ff0
Bryan Bishop <kanzure@gmail.com>
2014-08-14 19:52:12 +0000

    sleep after consul join for supervisor reasons
    After joining the consul cluster, sleep for a few seconds so that
    supervisor will think that everything is fine. Exit immediately if it
    failed, so that supervisor will know to retry.
    
    see #44
    
    original-commit-id: ec90c6732cfdb4e13cbe00667dfaa9e7a9886b8a

--- consuljoin.sh
+++ consuljoin.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 echo "consul join \$CONSULSERVER_1_PORT_8500_TCP_ADDR ($CONSULSERVER_1_PORT_8500_TCP_ADDR)"
 consul join $CONSULSERVER_1_PORT_8500_TCP_ADDR
+
+if [ $? -eq 0 ]; then
+    echo OK
+
+    # sleep so supervisor knows that it worked
+    sleep 2
+else
+    exit 1
+fi
